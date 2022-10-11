9253bbef327b5b6b7b97efc971d81335e4299e8a
theAnvil01 <99872866+theAnvil01@users.noreply.github.com>
2022-10-11 22:03:53 +0530

    updated with sublist3r tool.

--- reconage.sh
+++ reconage.sh
@@ -10,4 +10,10 @@ echo ""
 echo ""
 echo "starting amass"
 amass enum  -d $dom -o amass.$dom.txt >> /dev/null
+echo "amass has finished its task"
+echo ""
+echo ""
+echo "Starting Sublist3r"
+  sublist3r -d $dom -o sublist3r.$dom.txt >>/dev/null
+echo "Sublist3r finished its task" 
 echo amass.$dom.txt | sort -u | subdomain.txt
